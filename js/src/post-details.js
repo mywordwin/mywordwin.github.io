a37702be8260b32ddf01bdd56ec37be26f1740e4
mywordwin <2361619726@qq.com>
2019-07-06 15:15:31 +0800

    Site updated: 2019-07-06 15:15:21

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -97,3 +97,14 @@ $(document).ready(function () {
       }) : NexT.utils.displaySidebar();
   }
 });
+
+// 添加代码折叠
+$(document).ready(function(){
+  $(document).on('click', '.fold_hider', function(){
+      $('>.fold', this.parentNode).slideToggle();
+      $('>:first', this).toggleClass('open');
+  });
+  //默认情况下折叠
+  $("div.fold").css("display","none");
+});
+
